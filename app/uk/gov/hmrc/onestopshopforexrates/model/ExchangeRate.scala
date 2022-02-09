3a557a0b32c0d74747ff8ed41e53b70b65c0dd76
oliwiaKot <48218839+oliwiaKot@users.noreply.github.com>
2022-02-09 15:04:33 +0000

    VEOSS-796| added a connector to retrieve last 5 days of exchange rates

--- app/uk/gov/hmrc/onestopshopforexrates/model/ExchangeRate.scala
+++ app/uk/gov/hmrc/onestopshopforexrates/model/ExchangeRate.scala
@@ -1,5 +1,54 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.onestopshopforexrates.model
 
-import java.time.LocalDateTime
+import play.api.libs.json.{OFormat, OWrites, Reads, __}
+import uk.gov.hmrc.mongo.play.json.formats.MongoJavatimeFormats
 
-case class ExchangeRate(date: LocalDateTime, baseCurrency: String, targetCurrency: String, value: BigDecimal)
+import java.time.{LocalDate, LocalDateTime}
+
+case class ExchangeRate(date: LocalDate, baseCurrency: String, targetCurrency: String, value: BigDecimal)
+
+object ExchangeRate {
+
+  val reads: Reads[ExchangeRate] = {
+
+    import play.api.libs.functional.syntax._
+
+    (
+      (__ \ "date").read(MongoJavatimeFormats.localDateFormat) and
+        (__ \ "baseCurrency").read[String] and
+        (__ \ "targetCurrency").read[String] and
+        (__ \ "value").read[BigDecimal]
+      ) (ExchangeRate.apply _)
+  }
+
+  val writes: OWrites[ExchangeRate] = {
+
+    import play.api.libs.functional.syntax._
+
+    (
+      (__ \ "date").write(MongoJavatimeFormats.localDateFormat) and
+        (__ \ "baseCurrency").write[String] and
+        (__ \ "targetCurrency").write[String] and
+        (__ \ "value").write[BigDecimal]
+      ) (unlift(ExchangeRate.unapply))
+  }
+
+  implicit val format: OFormat[ExchangeRate] = OFormat(reads, writes)
+
+}
--- build.sbt
+++ build.sbt
@@ -27,6 +27,35 @@ lazy val microservice = Project(appName, file("."))
   )
   .settings(publishingSettings: _*)
   .configs(IntegrationTest)
-  .settings(integrationTestSettings(): _*)
+  .settings(inConfig(IntegrationTest)(itSettings): _*)
+  .configs(Test)
+  .settings(inConfig(Test)(testSettings): _*)
   .settings(resolvers += Resolver.jcenterRepo)
   .settings(PlayKeys.playDefaultPort := 10199)
+
+
+lazy val itSettings = Defaults.itSettings ++ Seq(
+  unmanagedSourceDirectories := Seq(
+    baseDirectory.value / "it",
+    baseDirectory.value / "test" / "generators"
+  ),
+  unmanagedResourceDirectories := Seq(
+    baseDirectory.value / "it" / "resources"
+  ),
+  parallelExecution := false,
+  fork := true,
+  javaOptions ++= Seq(
+    "-Dlogger.resource=logback-it.xml"
+  )
+)
+
+lazy val testSettings = Defaults.testSettings ++ Seq(
+  unmanagedSourceDirectories := Seq(
+    baseDirectory.value / "test"
+  ),
+  parallelExecution := false,
+  fork := true,
+  javaOptions ++= Seq(
+    "-Dlogger.resource=logback-test.xml"
+  )
+)
